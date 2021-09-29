3b2bcb70a3f30adfcd5bdfaea26babd6da264151
xiangshaoxiong <xiangshaoxiong@wavewisdom.com>
2021-09-29 14:32:45 +0800

    [MAIN][一个消息队列的模型]

--- Simulator_Linux/src/main.c
+++ Simulator_Linux/src/main.c
@@ -12,61 +12,7 @@
 #include "uart5.h"
 #include "uart8.h"
 
-//#define  TEST
 
-#ifdef TEST
-typedef struct
-{
-    int ID;
-    portCHAR Data[ 20 ];
-} Message, *pMessage;
-
-xQueueHandle MsgQueue;
-void TaskA( void *pvParameters );
-void TaskB( void *pvParameters );
-static void vSenderTask(void *pvParameters){
-    printf("创建发送TASK \n");
-    Message message;
-    message.ID=1;
-    strcpy(message.Data, "hello");
-    portBASE_TYPE  xStatus;
-
-    for (;  ;) {
-
-        xStatus = xQueueSend( MsgQueue, (void *)&message, 0 );
-        if(xStatus != pdPASS ){
-            printf("could not send to the queue \r\n");
-
-        }else{
-            printf("send ID =%d , DATA =%s\r\n", message.ID, message.Data );
-        }
-        taskYIELD();
-        message.ID++;
-    }
-
-}
-static void vReceiverTask(void *pvParamters){
-    printf("创建接收TASK \n");
-
-    Message message;
-    portBASE_TYPE  xStatus;
-    const portTickType xTicksToWait = 100/portTICK_RATE_MS;
-
-    for (;  ;) {
-        if (uxQueueMessagesWaiting(MsgQueue) != 0) {
-            printf("queue should have been empty ! \r\n");
-        }
-
-        xStatus = xQueueReceive(MsgQueue, &message, xTicksToWait);
-        if (xStatus == pdPASS) {
-            printf("received ID = %d, DATA= %s \r\n", message.ID, message.Data);
-        } else {
-            printf("could not receive from the queue \r\n");
-        }
-    }
-}
-
-#else
 
 void Uart5_Task(void *pvParameters) {
 
@@ -77,7 +23,6 @@ void Uart5_Task(void *pvParameters) {
 void Uart8_Task(void *pvParameters) {
     uart8_task_start();
 }
-#endif
 
 int main(void ){
     printf("创建两个TASK \n");
